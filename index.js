e751593bfe2714c6f1d37e290210bd7c24d75a0a
viploveuser <viploveuser@LnuxUbuntu.myguest.virtualbox.org>
2023-05-25 22:28:39 +0530

    added the Edit and delet button.

--- index.js
+++ index.js
@@ -44,6 +44,21 @@ window.addEventListener('load', () => {
 		list_el.appendChild(task_el);
 
         input.value = '';
+
+        task_edit_el.addEventListener('click', (e) => {
+            if (task_edit_el.innerText.toLowerCase() === 'edit'){
+                task_edit_el.innerText = 'Save';
+                task_input_el.removeAttribute('readonly');
+                task_input_el.focus();
+            }else {
+                task_edit_el.innerText = 'Edit';
+                task_input_el.setAttribute('readonly', 'readonly');
+            }
+        } )
+
+        task_delete_el.addEventListener('click', (e) => {
+            list_el.removeChild(task_el);
+        })
     })
 })
 
